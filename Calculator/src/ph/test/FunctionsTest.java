7eef11f28cff0392cc40f75fed4ad771b5072dd2
Drew <asc2808@rit.edu>
2015-05-03 18:53:59 -0400

    trying to build

--- Calculator/src/ph/test/FunctionsTest.java
+++ Calculator/src/ph/test/FunctionsTest.java
@@ -1,8 +1,10 @@
-package ph.calculator;
+package ph.calculator.test;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import ph.calculator.Functions;
 
 public class FunctionsTest {
 
